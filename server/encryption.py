4e1de4aa5d687fa780ad31ed24d23843d2699c4d
TheHoodieAfro <crissanchezpelaez@gmail.com>
2022-11-08 14:54:31 -0500

    fixed

--- server/encryption.py
+++ server/encryption.py
@@ -1,4 +1,7 @@
+import os
+
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 
 def generateKeyPair():
@@ -13,7 +16,53 @@ def generateKeyPair():
     file_out.write(public_key)
     file_out.close()
 
+def getPrivateKey():
+    f = open('./keys/private.pem"','r')
+    private_key = RSA.import_key(f.read())
+    f.close()
+
+    return private_key
+
+def getPublicKey():
+    f = open('./keys/public.pem"','r')
+    public_key = RSA.import_key(f.read())
+    f.close()
+
+    return public_key
+
 
 def decryptFile(file):
+
+    count_files = 0
+    dir_path = './data'
+    for path in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, path)):
+            count_files += 1
+
+    if count_files >= 100:
+        for path in os.listdir(dir_path):
+            os.remove(os.path.join(dir_path, path))
+
+    private_key = getPrivateKey()
+    crypted_session, nonce, tag, ciphertext = [ file.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+
+    # Decrypt the session key with the private RSA key
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    session = cipher_rsa.decrypt(crypted_session)
+
+    # Decrypt the data with the AES session key
+    cipher_aes = AES.new(session, AES.MODE_EAX, nonce)
+    data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+
+    c = 0
+    while True:
+        path = "./data/file{}.txt".format(c)
+        if os.path.exists(path):
+            decryptedFile = open(path, "a")
+            break
+        c += 1
+
+    decryptedFile.write(data.decode("utf-8"))
+    decryptedFile .close()
 
     return decryptedFile
