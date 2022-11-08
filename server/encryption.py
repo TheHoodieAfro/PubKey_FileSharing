4ce0284aecec46aca896bc35b39c7735df673a56
TheHoodieAfro <crissanchezpelaez@gmail.com>
2022-11-08 13:42:03 -0500

    Added: creation of keys

---
+++ server/encryption.py
@@ -0,0 +1,19 @@
+from Crypto.PublicKey import RSA
+
+
+def generateKeyPair():
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    file_out = open("./keys/private.pem", "wb")
+    file_out.write(private_key)
+    file_out.close()
+
+    public_key = key.publickey().export_key()
+    file_out = open("./keys/public.pem", "wb")
+    file_out.write(public_key)
+    file_out.close()
+
+
+def decryptFile(file):
+
+    return decryptedFile
